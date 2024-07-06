b4461c10534fc8796a2e578bd91dd91abf18539b
SammyMwaura <mwaurasammy500@gmail.com>
2024-07-06 22:25:53 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,39 +1,70 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+let shoppingList = [];
+
 function addItems() {
-    if (inputBox.value === '') {
-        alert("You must enter an item!");
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = '';
+  if (inputBox.value === "") {
+    alert("You must enter an item!");
+  } else {
+    let item = {
+      name: inputBox.value.trim(),
+      checked: false,
+    };
+    shoppingList.push(item);
+    updateList();
     saveData();
+  }
+  inputBox.value = "";
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      let li = e.target;
+      let index = Array.from(listContainer.children).indexOf(li);
+      shoppingList[index].checked = !shoppingList[index].checked;
+      updateList();
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      let li = e.target.parentElement;
+      let index = Array.from(listContainer.children).indexOf(li);
+      shoppingList.splice(index, 1);
+      updateList();
+      saveData();
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
+  },
+  false
+);
+
+function clearItems() {
+  shoppingList = [];
+  updateList();
+  saveData();
+}
+
+function saveData() {
+  localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+}
+
+function showData() {
+  shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+  updateList();
+}
+
+function updateList() {
+  listContainer.innerHTML = "";
+  shoppingList.forEach((item, index) => {
+    let li = document.createElement("li");
+    li.textContent = item.name;
+    if (item.checked) {
+      li.classList.add("checked");
     }
-}, false);
-
-function clearItems(){
-    listContainer.innerHTML = '';
+    let span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li.appendChild(span);
+    listContainer.appendChild(li);
+  });
 }
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-function showData(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
 showData();
